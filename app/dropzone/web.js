f4895fad722c4d517c9dfdad248d5493d0ead44c
Bram van der Veen <96aa48@gmail.com>
2015-08-05 19:47:30 +0200

    Added a lot of features (preview and more) to the web frontend.

--- app/dropzone/web.js
+++ app/dropzone/web.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express('express');
 var less = require('express-less');
+var fs = require('fs');
 
 //import self-written modules.
 var database = require('./database');
@@ -11,7 +12,7 @@ var config = require('./settings');
 //setting up app specific settings
 app.set('view engine', 'jade');
 app.set('views', process.cwd() + '/web/views');
-app.use('/less', less(process.cwd() + '/web/less'));
+app.use('/less', less(process.cwd() + '/web/less', {debug: true}));
 app.use('/js', express.static(process.cwd() + '/web/js'));
 
 app.get('/', function (req, res) {
@@ -20,15 +21,25 @@ app.get('/', function (req, res) {
   });
 });
 
-app.get('/download/:file', function (req, res, next) {
+app.get('/download/:download_file', function (req, res, next) {
   next();
 });
 
-app.param('file', function (req, res, next, file) {
-  database.get(file, function (database_entry) {
+app.param('download_file', function (req, res, next, download_file) {
+  database.get(download_file, function (database_entry) {
     res.sendFile(database_entry.path, {headers: {'Content-Type': 'application/octet-stream'}}, function (err) {
       if (err) console.warn(err);
     });
+  });
+});
+
+app.get('/preview/:preview_file', function (req, res, next) {
+    next();
+});
+
+app.param('preview_file', function (req, res, next, preview_file) {
+  database.get(preview_file, function (database_entry) {
+    res.render('preview', {'file': database_entry, 'content': fs.readFileSync(database_entry.path), 'utils': utils});
   });
 });
 
