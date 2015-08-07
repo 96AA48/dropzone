679da40bbf004ce252b3c916225c87fe26b3b12f
Bram van der Veen <96aa48@gmail.com>
2015-08-07 23:14:49 +0200

    Added a randomly generated background image to the web interface.

--- app/web/js/script.js
+++ app/web/js/script.js
@@ -1,3 +1,10 @@
-$('ul.list > li').click(function () {
+$('div.list ul > li').click(function () {
   $(this).attr('class', $(this).attr('class') == 'clicked' ? '' : 'clicked');
 });
+
+var pattern = Trianglify({
+  width: window.innerWidth,
+  height: window.innerHeight
+});
+
+pattern.canvas($('canvas')[0]);
