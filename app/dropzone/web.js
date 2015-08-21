fe15053b93cb5c4ed4c4a68f96f4b22e5f8e1936
Bram van der Veen <96aa48@gmail.com>
2015-08-21 21:06:26 +0200

    Added "pings" to let the app know about users in the web frontend

--- app/dropzone/web.js
+++ app/dropzone/web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express('express');
 var less = require('express-less');
 var fs = require('fs');
+var $ = require('jquery');
 
 //import self-written modules.
 var database = require('./database');
@@ -17,6 +18,7 @@ app.use('/js', express.static(process.cwd() + '/web/js'));
 app.disable('view cache');
 
 app.get('/', function (req, res) {
+  ping('connect');
   database.list(function (list) {
     res.render('home', {'list': list, 'utils': utils});
   });
@@ -27,6 +29,7 @@ app.get('/download/:download_file', function (req, res, next) {
 });
 
 app.param('download_file', function (req, res, next, download_file) {
+  ping('download');
   database.get(download_file, function (database_entry) {
     res.sendFile(database_entry.path, {headers: {'Content-Type': 'application/octet-stream'}}, function (err) {
       if (err) console.warn(err);
@@ -39,10 +42,43 @@ app.get('/preview/:preview_file', function (req, res, next) {
 });
 
 app.param('preview_file', function (req, res, next, preview_file) {
+  ping('preview');
   database.get(preview_file, function (database_entry) {
     var content = (!database_entry.type.match(/video|audio/g) && !utils.ignore_file(database_entry.name, database_entry.type)) ? fs.readFileSync(database_entry.path) : '';
     res.render('preview', {'file': database_entry, 'content': content, 'utils': utils});
   });
 });
+
+function ping(type) {
+  var ping = 'ping';
+  switch(type) {
+    case 'connect':
+      ping += ' connect';
+    break;
+
+    case 'preview':
+      ping += ' preview';
+    break;
+
+    case 'download':
+      ping += ' download';
+    break;
+  }
+
+  $('.cloud').toggleClass(ping);
+
+  setTimeout(function () {
+    console.log('ping timeout', ping);
+    $('.cloud').toggleClass(ping);
+  }, 250);
+}
+
+function feedback(str) {
+  $('#feedback span').text(str);
+  $('#feedback').addClass('show');
+  setTimeout(function () {
+    $('#feedback').removeClass('show');
+  }, 1500);
+}
 
 module.exports = function () {app.listen(config.get().port, config.get().use_custom_host ? config.get().host : null);};
