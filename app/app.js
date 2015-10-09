c5f72c94978377c56dd941498d8ae01749178615
Bram van der Veen <96aa48@gmail.com>
2015-10-09 14:06:41 +0200

    Moved all modules to the app dir

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 console.log('Loaded app.js');
 global.gui = require('nw.gui');
 
-require('./dropzone/frontend');
-require('./dropzone/web').start();
+require('./frontend');
+require('./web').start();
